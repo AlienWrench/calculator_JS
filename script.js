9bd4888bfe0c315a9dec62a670bc437c93a7b961
Cody Allen <AlienWrench46@gmail.com>
2022-08-30 12:13:17 -0600

    Add operate function. *Note: All functions up to this point have been tested in dev console and seem to return appropriate/desired answersadd script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //variables/constants here please
+let numStringInput1 = "0";
+let numStringInput2 = "";
+
+let operatorSign = "";
+
+let solution;       //keep as number, return to string? does it matter?
 
 //works in dev console
 function add(num1, num2) {  //remember, despite their parameter names, these arguments will be strings!
@@ -20,6 +26,7 @@ function subtract(num1, num2) {
     return difference;
 }
 
+//seems to work in dev console
 function multiply(num1, num2) {
     const multiplicand = Number(num1);
     const multiplier = Number(num2);
@@ -29,6 +36,7 @@ function multiply(num1, num2) {
     return product;
 }
 
+//seems to work in dev console
 function divide (num1, num2) {
     const dividend = Number(num1);
     const divisor = Number(num2);
@@ -36,4 +44,32 @@ function divide (num1, num2) {
     const quotient = dividend / divisor;
 
     return quotient;
+}
+
+//seems to work in dev console 
+function operate(operatorSign) {    //would it be/look better if this function took the numbers as parameters and everything else stayed the same? change variable names below too obviously
+    let solution = 0;
+    
+    if (numStringInput2 !== "") {
+        switch (operatorSign) {
+            case "+":
+                solution = add(numStringInput1, numStringInput2);
+                break;
+
+            case "-":
+                solution = subtract(numStringInput1, numStringInput2);
+                break;
+
+            case "*":
+                solution = multiply(numStringInput1, numStringInput2);
+                break;
+
+            case "/":
+                solution = divide(numStringInput1, numStringInput2);
+                break;
+        }
+
+        return solution;
+    }
+    else return numStringInput1;
 }
