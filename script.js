1191b0a40a5b3b024d2fcb49fec187c8765901fa
Cody Allen <AlienWrench46@gmail.com>
2022-08-30 11:50:41 -0600

    Finish four basic calculator functions (add, subtract, multiply, divide), change 'solution' variable names in said functions.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //variables/constants here please
 
 //works in dev console
-function add (num1, num2) {  //remember, despite their parameter names, these arguments will be strings!
+function add(num1, num2) {  //remember, despite their parameter names, these arguments will be strings!
     const addend1 = Number(num1);
     const addend2 = Number(num2);
 
-    const solution = addend1 + addend2;
+    const sum = addend1 + addend2;
 
-    return solution;
+    return sum;
 }
 
 //also works in dev console
@@ -15,7 +15,25 @@ function subtract(num1, num2) {
     const minuend = Number(num1);
     const subtrahend = Number(num2);
 
-    const solution = minuend - subtrahend;
+    const difference = minuend - subtrahend;
 
-    return solution;
+    return difference;
+}
+
+function multiply(num1, num2) {
+    const multiplicand = Number(num1);
+    const multiplier = Number(num2);
+
+    const product = multiplicand * multiplier;
+
+    return product;
+}
+
+function divide (num1, num2) {
+    const dividend = Number(num1);
+    const divisor = Number(num2);
+
+    const quotient = dividend / divisor;
+
+    return quotient;
 }
